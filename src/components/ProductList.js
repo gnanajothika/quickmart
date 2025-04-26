13516c0c086921e2b4641e39f65d85175962f7b6
Gnanajothika <jothikakanagaraj@gmail.com>
2025-04-26 15:27:32 +0530

    dynamically used product images from assets

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -4,19 +4,28 @@ const ProductList = ({ products, addToCart }) => {
   return (
     <div className="product-section">
       <h2>🛍 Products</h2>
-      {products.map((product) => (
-        <div key={product.id} className="product-card">
-          {/* <img
-            src={`https://via.placeholder.com/250x250?text=${product.name}`}
-            alt={product.name}
-          /> */}
-          <h3>{product.name}</h3>
-          <p>${product.price}</p>
-          <button onClick={() => addToCart(product)}>Add to Cart</button>
-        </div>
-      ))}
+      {products.map((product) => {
+        let imagePath = extractImagePath(product.img);
+
+        return (
+          <div key={product.id} className="product-card">
+            {imagePath && <img src={imagePath} alt={product.name} />}
+            <h3>{product.name}</h3>
+            <p>${product.price}</p>
+            <button onClick={() => addToCart(product)}>Add to Cart</button>
+          </div>
+        );
+      })}
     </div>
   );
+};
+
+const extractImagePath = (imageName) => {
+  try {
+    return require(`./../assets/product-images/${imageName}`);
+  } catch {
+    return null;
+  }
 };
 
 export default ProductList;
